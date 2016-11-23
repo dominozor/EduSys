767135354576406976e6e22d6695e0a69cd11988
ATA <e1942002@ceng.metu.edu.tr>
2016-11-23 03:20:10 +0200

    implementin Student Class and its update function according to average coordinates and distance of the face

--- classifier_webcam.py
+++ classifier_webcam.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 
 import time
-
+import copy
 start = time.time()
 
 import argparse
@@ -18,7 +18,81 @@ fileDir = os.path.dirname(os.path.realpath(__file__))
 modelDir = os.path.join(fileDir, 'models')
 dlibModelDir = os.path.join(modelDir, 'dlib')
 openfaceModelDir = os.path.join(modelDir, 'openface')
+unknownCounter=0
+globalPersons = [] #the list that keeps the information about who appeared on the frame
 
+def findIndex(person,globalPersons): #finds the index of a name in the objects list
+	for j in range(len(globalPersons)):
+		if person == globalPersons[j].name:
+			return j
+	return -1
+
+
+
+def distanceFromPoints(point1,point2): #given two pixel coordinates of the top and the bottom of the rectangle in someone's face, finds the distance of this person from the camera in centimeters
+	heigth=point1-point2
+	realcm=0
+	realcm=68*63/heigth
+	return realcm
+
+def updateGlobalList(persons,confidences,points): #from the names of the persons on a frame, updates the object list in terms of a student object with a name, distance and coordinates of his face
+	for i in persons:
+		flag=0
+		for j in globalPersons:
+			if i == j.name:
+				flag=1
+		if(flag==0):
+			tempStudent=Student(i)
+			globalPersons.append(tempStudent) #if the person with this name doesnt exist on the list, appends an object with this name
+	for i in range(len(persons)): #updates every student object in the list 
+		index=findIndex(persons[i],globalPersons)
+		distanceInCm=distanceFromPoints(points[i][1][1],points[i][0][1])
+		globalPersons[index].update(distanceInCm,points[i])
+
+def isSame(pointsi,globallisti): #finds if an unknown frame is the same as a known person by checking the similarity of their coordinates
+    if(pointsi[0][0]-globallisti.avLeft<5 and pointsi[0][1]-globallisti.avTop<5 and pointsi[1][0]-globallisti.avRight<5 and pointsi[1][1]-globallisti.avBottom<5):
+        return True
+    else:
+        return False
+
+class Student: 
+    def __init__(self,name):
+        self.name=name
+        self.averageDistance=0
+        self.numberOfDistances=0
+        self.avTop=0
+        self.avBottom=0
+        self.avRight=0
+        self.avLeft=0
+    def update (self,distance,pointsi): #average calculation of distance and coordinates of face
+        temp=self.averageDistance*self.numberOfDistances
+        temp1=self.avBottom*self.numberOfDistances
+        temp2=self.avTop*self.numberOfDistances
+        temp3=self.avLeft*self.numberOfDistances
+        temp4=self.avRight*self.numberOfDistances
+        self.numberOfDistances=self.numberOfDistances+1
+        temp=temp+distance
+        temp1=temp1+pointsi[1][1]
+        temp2=temp2+pointsi[0][1]
+        temp3=temp3+pointsi[0][0]
+        temp4=temp4+pointsi[1][0]
+
+        temp=temp*1.0/self.numberOfDistances
+        temp1=temp1*1.0/self.numberOfDistances
+        temp2=temp2*1.0/self.numberOfDistances
+        temp3=temp3*1.0/self.numberOfDistances
+        temp4=temp4*1.0/self.numberOfDistances
+
+        self.averageDistance=temp
+
+
+        self.avBottom=temp1
+        self.avTop=temp2  #(i[0][0],i[0][1]),(i[1][0],i[1][1]) left top right bottom
+        self.avLeft=temp3
+        self.avRight=temp4
+
+
+		
 
 def getRep(bgrImg):
     start = time.time()
@@ -108,7 +182,7 @@ def infer(img, args):
             dist = np.linalg.norm(rep - clf.means_[maxI])
             print("  + Distance from the mean: {}".format(dist))
             pass
-    return (persons, confidences,points)
+    return (persons, confidences, points)
 
 
 if __name__ == '__main__':
@@ -137,7 +211,8 @@ if __name__ == '__main__':
         help='Capture device. 0 for latop webcam and 1 for usb webcam')
     parser.add_argument('--width', type=int, default=320)
     parser.add_argument('--height', type=int, default=240)
-    parser.add_argument('--threshold', type=float, default=0.5)
+    parser.add_argument('--time', type=int, default=15) #time option that determines how long the calculations and video will be
+    parser.add_argument('--threshold', type=float, default=0.65)
     parser.add_argument('--cuda', action='store_true')
     parser.add_argument('--verbose', action='store_true')
     parser.add_argument(
@@ -156,6 +231,7 @@ if __name__ == '__main__':
 
     # Capture device. Usually 0 will be webcam and 1 will be usb cam.
     video_capture = cv2.VideoCapture(args.captureDevice)
+
     #video_capture=cv2.imread('/home/ata/Downloads/envenon.jpg')
     '''for i in range(0,len(video_capture)):
 	for j in range(0,len(video_capture[0])):
@@ -174,15 +250,19 @@ if __name__ == '__main__':
 		else:
 			video_capture[i][j][2]=255'''
 
-    print type(video_capture)
+    #print type(video_capture)
     video_capture.set(3, args.width)
     video_capture.set(4, args.height)
-
+    counter=time.time() 
     confidenceList = []
     while True:
+        diff=time.time()-counter
+        print diff
+        if (diff>args.time):
+            break
         ret, frame = video_capture.read()
         #frame=video_capture
-        persons, confidences,points = infer(frame, args)
+        persons, confidences, points = infer(frame, args)
         try:
             # append with two floating point precision
             confidenceList.append('%.2f' % confidences[0])
@@ -191,11 +271,25 @@ if __name__ == '__main__':
             # We can simply ignore it.
             pass
 
-        for i, c in enumerate(confidences):
+        for i, c in enumerate(confidences): #TODO merging an unknown with a known face
             if c <= args.threshold:  # 0.5 is kept as threshold for known face.
-                persons[i] = "_unknown"
+                #index=findIndex("_unknown",globalPersons)
+                #if(index==-1):
+                persons[i]="_unknown"
+                #else:
+                    #if(not isSame(points[i],globalPersons[index])):
+                        #persons[i]="_unknown"+str(unknownCounter)
+                        #unknownCounter=unknownCounter+1
 
-        print "P: " + str(persons) + " C: " + str(confidences)
+
+        updateGlobalList(persons,confidences,points) #updates global list if there is a new person in the frame and also updates the distance
+
+
+        #print "P: " + str(persons) + " C: " + str(confidences)
+        for i in globalPersons:
+        	print i.name +" "+ str(i.averageDistance) +" " +str(i.avTop) +" " +str(i.avBottom) +" "+ str(i.avRight)+" " +str(i.avLeft)
+
+        print (" ")
 
                 # Print the person name and conf value on the frame
         cv2.putText(frame, "P: {} C: {}".format(persons, confidences),
@@ -204,11 +298,13 @@ if __name__ == '__main__':
         for i in points:
         	cv2.rectangle(frame,(i[0][0],i[0][1]),(i[1][0],i[1][1]), (0,255,0),2)
         	cv2.putText(frame, str(persons[c]) ,(i[1][0]+5, i[1][1]+5), cv2.FONT_HERSHEY_SIMPLEX, 0.5 ,(0,0,255) , 1 )
+        	cv2.putText(frame, str(globalPersons[findIndex(persons[c],globalPersons)].averageDistance) ,(i[1][0]+15 , i[1][1]+15), cv2.FONT_HERSHEY_SIMPLEX, 0.5 ,(0,0,255) , 1 )  #puts average distance on the rectangle
         	c=c+1
         cv2.imshow('', frame)
         # quit the program on the press of key 'q'
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     # When everything is done, release the capture
+
     video_capture.release()
     cv2.destroyAllWindows()
