312f6ac3e1181fd2a2db0283b2d29ed68f7b277a
Enver Evci <e1942085@ceng.metu.edu.tr>
2017-03-22 07:06:26 +0300

    excel adding button

---
+++ Server/src/main/webapp/views/exam/add-excel.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    var exam;
+    var len;
+    var user;
+
+    $.ajax({
+        url: '/views/utility/utility.js',
+        dataType: 'script',
+        async: false  // This option prevents this function to execute asynchronized
+    });
+
+    exam = JSON.parse(readCookie('examCourse'));
+    user = JSON.parse(readCookie('mainuser'));
+    len = readCookie(('length'));
+
+    var img = document.getElementById("studentImage"); //This puts the profile picture of the student to the home page.
+    img.src = String(user["ppic"]);
+
+    var img = document.getElementById("studentImage2"); //This puts the profile picture of the student to the home page.
+    img.src = String(user["ppic"]);
+
+    var img = document.getElementById("studentImage3"); //This puts the profile picture of the student to the home page.
+    img.src = String(user["ppic"]);
+
+    $('#studentName').html(user["name"] + " " + user["surname"])
+    $('#studentButtonName').html(user["name"] + " " + user["surname"])
+    $('#stuName').html(user["name"] + " " + user["surname"])
+
+
+    $("#add-excForm").html('<form action="rest/file/upload" method="post" enctype="multipart/form-data"> <p> Select a file : <input type="file" name="file" size="45" /> </p> <p>Target Upload Path : <input type="text" name="examID" /></p> <input type="submit" value="Upload It" /> </form>');
+
+
+
+    $("#backToExamListPage").click(function(){
+        window.location.replace("http://localhost:8080/templates/exam/exam-list.html");
+    });
+});
+
