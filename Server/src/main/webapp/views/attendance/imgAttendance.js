01a86b376ba563d35a1ce6d4e537b5255c645288
ONAT <e2035772@ceng.metu.edu.tr>
2017-04-16 21:43:39 +0300

    offline attendance frontend completed

--- Server/src/main/webapp/views/attendance/imgAttendance.js
+++ Server/src/main/webapp/views/attendance/imgAttendance.js
@@ -1,6 +1,7 @@
 /**
  * Created by enver on 4/12/17.
  */
+
 $(window).on('load', function() {
     // Animate loader off screen
     $(".myModal2").fadeOut("slow");
@@ -20,6 +21,11 @@ $(document).ready(function() {
         async: false  // This option prevents this function to execute asynchronized
     });
 
+    $.ajax({
+        url: 'http://malsup.github.com/jquery.form.js',
+        dataType: 'script',
+        async: false  // This option prevents this function to execute asynchronized
+    });
 
     $.ajax({
         url: '/views/utility/utility.js',
@@ -30,6 +36,10 @@ $(document).ready(function() {
     var course = JSON.parse(readCookie('course'));
     var user = JSON.parse(readCookie('mainuser'));
 
+    $('#studentName').html(user["name"] + " " + user["surname"]);
+    $('#studentButtonName').html(user["name"] + " " + user["surname"]);
+    $('#stuName').html(user["name"] + " " + user["surname"]);
+
     var img = document.getElementById("studentImage"); //This puts the profile picture of the student to the home page.
     img.src = String(user["ppic"]);
 
@@ -39,27 +49,37 @@ $(document).ready(function() {
     var img = document.getElementById("studentImage3"); //This puts the profile picture of the student to the home page.
     img.src = String(user["ppic"]);
 
+    document.getElementById("contentHeader").innerHTML = '<h1>' + course["id"] + " " + course["name"] + " / Section " + course["sectionId"] + '</h1>';
+
     $("#course-home-btn").click(function(){
         window.location.reload();
     });
 
-    $(function() {
-        $('#upload-form').ajaxForm({
-            success: function(msg) {
-                if(msg){
-                    var backLink = JSON.parse(window.localStorage.getItem("backLink")); // Retrieving
-                    window.location.replace(backLink);
-                }
+    $("#courseID").val(course["id"]);
+    $("#sectionNo").val(course["sectionId"]);
 
+    jQuery("#upload-form").submit(function(e){
+        //The following stops the form from redirecting
+        e.preventDefault();
+        document.getElementById('loading-gif').style.display = 'block';
+        jQuery("#upload-form").ajaxSubmit({
+            type: 'POST',
+            url: 'http://localhost:8080/rest/upload/attendanceImage',
+            data: jQuery('#upload-form').serialize(),
+            success: function (msg) {
+                //The data variable will contain the response data
+                //if it's successful, you can no redirect wherever you want
+                document.getElementById('loading-gif').style.display = 'none';
+                window.location.href = "http://localhost:8080/templates/course/course-home.html";
             },
             error: function(msg) {
-                $("#upload-error").text("Couldn't upload file");
+
+                $("#error_rgs_msg").html("<b style='color:red'>Fail...</b>");
             }
         });
     });
 
-    $("#backPage").click(function(){
-        var backLink = JSON.parse(window.localStorage.getItem("backLink")); // Retrieving
-        window.location.replace(backLink); //redirects back to lecturer page
+    $("#backToCourseHomePage").click(function(){
+        window.location.replace("http://localhost:8080/templates/course/course-home.html");
     });
 });
