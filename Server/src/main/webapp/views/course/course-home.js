81ceebb680e9ae29f09ae8e6066b37e3d2698c92
e1942002 <e1942002@ceng.metu.edu.tr>
2017-04-17 03:37:30 +0300

    Seating Chart is given real values

--- Server/src/main/webapp/views/course/course-home.js
+++ Server/src/main/webapp/views/course/course-home.js
@@ -260,64 +260,11 @@ $(document).ready(function() {
 
 
 
-    var seatPercentageListObj = getSeatingPercentageForCourse(course["id"],course["sectionId"]);
-    var seatPercentageList = JSON.parse(seatPercentageListObj.responseText);
-    var lenSeat = seatPercentageList.length;
+    //var seatPercentageListObj = getSeatingPercentageForCourse(course["id"],course["sectionId"]);
+    //var seatPercentageList = JSON.parse(seatPercentageListObj.responseText);
+    //var lenSeat = seatPercentageList.length;
 
-
-    //-------------
-    //- PIE CHART -
-    //-------------
-    // Get context with jQuery - using jQuery's .get() method.
-    var pieChartCanvas = $("#pieChart").get(0).getContext("2d");
-    var pieChart = new Chart(pieChartCanvas);
-    var PieData = [
-        {
-            value: 20,
-            color: "#f56954",
-            highlight: "#f56954",
-            label: "First Three Rows"
-        },
-        {
-            value: 30,
-            color: "#00a65a",
-            highlight: "#00a65a",
-            label: "Middle Three Rows"
-        },
-        {
-            value: 10,
-            color: "#f39c12",
-            highlight: "#f39c12",
-            label: "Back Three Rows"
-        },
-    ];
-    var pieOptions = {
-        //Boolean - Whether we should show a stroke on each segment
-        segmentShowStroke: true,
-        //String - The colour of each segment stroke
-        segmentStrokeColor: "#fff",
-        //Number - The width of each segment stroke
-        segmentStrokeWidth: 2,
-        //Number - The percentage of the chart that we cut out of the middle
-        percentageInnerCutout: 50, // This is 0 for Pie charts
-        //Number - Amount of animation steps
-        animationSteps: 100,
-        //String - Animation easing effect
-        animationEasing: "easeOutBounce",
-        //Boolean - Whether we animate the rotation of the Doughnut
-        animateRotate: true,
-        //Boolean - Whether we animate scaling the Doughnut from the centre
-        animateScale: false,
-        //Boolean - whether to make the chart responsive to window resizing
-        responsive: true,
-        // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-        maintainAspectRatio: true,
-        //String - A legend template
-        legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
-    };
-    //Create pie or doughnut chart
-    // You can switch between pie and douhnut using the method below.
-    pieChart.Doughnut(PieData, pieOptions);
+    //console.log(lenSeat);
 
 
     var newgraphlist=[];
@@ -428,7 +375,7 @@ $(document).ready(function() {
     var lecturerSeatingObj=getAllSeatingsForLecturerCourse(user["id"],course["sectionId"], course["id"]);
     var lecturerSeatingList=JSON.parse(lecturerSeatingObj.responseText);
     console.log(lecturerSeatingList);
-
+    var pieChartArray =[];
     var seatCounter=0;
 
     for(var i = 0; i<lecturerSeatingList.length;i++)
@@ -440,6 +387,7 @@ $(document).ready(function() {
                 var seat;
                 seat=Math.floor(lecturerSeatingList[i]["leftcoor"]/20);
                 zz[j][seat] += 1;
+                pieChartArray.push(j);
                 seatCounter++;
             }
         }
@@ -481,6 +429,118 @@ $(document).ready(function() {
     ];
 
     Plotly.newPlot('myDiv', data);
+
+
+    console.log(zz);
+    console.log(numberOfRow);
+
+    var firstrows,secondrows,thirdrows;
+    var fr=0;
+    var sr=0;
+    var tr=0;
+
+    if(numberOfRow%3==0)
+    {
+        firstrows=numberOfRow/3;
+        secondrows=numberOfRow/3;
+        thirdrows=numberOfRow/3;
+    }
+
+    if(numberOfRow%3==1)
+    {
+        firstrows=Math.floor(numberOfRow/3);
+        secondrows=Math.floor(numberOfRow/3);
+        thirdrows=Math.ceil(numberOfRow/3);
+    }
+
+
+    if(numberOfRow%3==2)
+    {
+        firstrows=Math.floor(numberOfRow/3);
+        secondrows=Math.ceil(numberOfRow/3);
+        thirdrows=Math.ceil(numberOfRow/3);
+    }
+    console.log(pieChartArray);
+    console.log( firstrows + " " + secondrows + " " + thirdrows);
+
+    for(var i=0;i<pieChartArray.length;i++)
+    {
+        if(pieChartArray[i]< firstrows)
+        {
+            fr++;
+        }
+        else if((pieChartArray[i] >= firstrows) && (pieChartArray[i]<(secondrows+firstrows)))
+        {
+            sr++;
+        }
+        else
+        {
+            tr++;
+        }
+    }
+    console.log(fr + " " + sr + " " + tr);
+
+    //-------------
+    //- PIE CHART -
+    //-------------
+    // Get context with jQuery - using jQuery's .get() method.
+    var pieChartCanvas = $("#pieChart").get(0).getContext("2d");
+    var pieChart = new Chart(pieChartCanvas);
+    var PieData = [
+        {
+            value: fr,
+            color: "#f56954",
+            highlight: "#f56954",
+            label: "First Three Rows"
+        },
+        {
+            value: sr,
+            color: "#00a65a",
+            highlight: "#00a65a",
+            label: "Middle Three Rows"
+        },
+        {
+            value: tr,
+            color: "#f39c12",
+            highlight: "#f39c12",
+            label: "Back Three Rows"
+        },
+    ];
+    var pieOptions = {
+        //Boolean - Whether we should show a stroke on each segment
+        segmentShowStroke: true,
+        //String - The colour of each segment stroke
+        segmentStrokeColor: "#fff",
+        //Number - The width of each segment stroke
+        segmentStrokeWidth: 2,
+        //Number - The percentage of the chart that we cut out of the middle
+        percentageInnerCutout: 50, // This is 0 for Pie charts
+        //Number - Amount of animation steps
+        animationSteps: 100,
+        //String - Animation easing effect
+        animationEasing: "easeOutBounce",
+        //Boolean - Whether we animate the rotation of the Doughnut
+        animateRotate: true,
+        //Boolean - Whether we animate scaling the Doughnut from the centre
+        animateScale: false,
+        //Boolean - whether to make the chart responsive to window resizing
+        responsive: true,
+        // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+        maintainAspectRatio: true,
+        //String - A legend template
+        legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
+    };
+    //Create pie or doughnut chart
+    // You can switch between pie and douhnut using the method below.
+    pieChart.Doughnut(PieData, pieOptions);
+
+
+
+
+
+
+
+
 
 
 });
