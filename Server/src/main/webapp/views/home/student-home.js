6ec7fcdda16119a4abe7e5324d365f2e82905572
onur <e1941665@ceng.metu.edu.tr>
2017-05-03 00:30:42 +0300

    Optimizing student home page, career path front end and some debugging.

--- Server/src/main/webapp/views/home/student-home.js
+++ Server/src/main/webapp/views/home/student-home.js
@@ -34,6 +34,11 @@ function getAttendanceCountsOfSection(userid) {
 
 var graphList=[];
 
+$(window).on('load', function() {
+    // Animate loader off screen
+    $(".myModal2").fadeOut("slow");
+});
+
 
 $(document).ready(function() {
 
@@ -157,7 +162,7 @@ $(document).ready(function() {
 
     //Pie Chart Of Ratio Of Interests
 
-    $('#pieChartInterestCaller').one("click", function () {
+    //$('#pieChartInterestCaller').one("click", function () {
 
         var avgInterestInfoObj = listAverageInterestInfo(user["id"]);
         var avgInterestInfo = JSON.parse(avgInterestInfoObj.responseText);
@@ -283,11 +288,11 @@ $(document).ready(function() {
                 }
             }
         });
-    });
+    //});
 
 
     //Line Chart Of The Ratio Of Interests
-    $('#lineChartInterestCaller').one("click", function () {
+    //$('#lineChartInterestCaller').one("click", function () {
         $('#lineChartInterest').attr("style","height:505px");
 
         var graphInfo = [];
@@ -394,13 +399,13 @@ $(document).ready(function() {
             chart.draw(data, options);
         }
 
-    });
+    //});
 
 
 
     //Pie Chart Of Ratio Of Attendances
 
-    $('#pieChartAttendanceCaller').one("click", function () {
+    //$('#pieChartAttendanceCaller').one("click", function () {
 
         var coursesObj = listCoursesOfStudent(user["id"]);
         var courses = JSON.parse(coursesObj.responseText);
@@ -412,7 +417,7 @@ $(document).ready(function() {
             var courAttListObj = getCourseAttForStudent(user["id"], courses[i]["id"]);
             var courAttList = JSON.parse(courAttListObj.responseText);
 
-            var totalNumberOfAttendanceObj = getNumberOfAttendance(courses[i]["id"], courses[i]["sectionNo"]);
+            var totalNumberOfAttendanceObj = getNumberOfAttendance(courses[i]["id"], courses[i]["sectionId"]);
             var totalNumberOfAttendance = JSON.parse(totalNumberOfAttendanceObj.responseText);
 
             var letters = '0123456789ABCDEF';
@@ -481,12 +486,12 @@ $(document).ready(function() {
                 }
             }
         });
-    });
+    //});
 
 
     //Heat Map!!
 
-    $('#heatMapCaller').one("click", function () {
+    //$('#heatMapCaller').one("click", function () {
 
         var courseInterestObj = getInterestsOfCourses(user["id"]);
         var courseInterest = JSON.parse(courseInterestObj.responseText);
@@ -597,7 +602,257 @@ $(document).ready(function() {
         ];
 
         Plotly.newPlot('heatmapContainer', data);
-    });
+    //});
+
+
+
+    var labelType, useGradients, nativeTextSupport, animate;
+
+    (function() {
+        var ua = navigator.userAgent,
+            iStuff = ua.match(/iPhone/i) || ua.match(/iPad/i),
+            typeOfCanvas = typeof HTMLCanvasElement,
+            nativeCanvasSupport = (typeOfCanvas == 'object' || typeOfCanvas == 'function'),
+            textSupport = nativeCanvasSupport
+                && (typeof document.createElement('canvas').getContext('2d').fillText == 'function');
+        //I'm setting this based on the fact that ExCanvas provides text support for IE
+        //and that as of today iPhone/iPad current text support is lame
+        labelType = (!nativeCanvasSupport || (textSupport && !iStuff))? 'Native' : 'HTML';
+        nativeTextSupport = labelType == 'Native';
+        useGradients = nativeCanvasSupport;
+        animate = !(iStuff || !nativeCanvasSupport);
+    })();
+
+    var Log = {
+        elem: false,
+        write: function(text){
+            if (!this.elem)
+                this.elem = document.getElementById('log');
+            this.elem.innerHTML = text;
+            this.elem.style.left = (500 - this.elem.offsetWidth / 2) + 'px';
+        }
+    };
+
+
+    //function init(){
+        // init data
+        var json = [
+            {
+                "adjacencies": [
+                    {
+                        "nodeTo": "node1",
+                        "nodeFrom": "node0",
+                        "data": {
+                            "$color": "#557EAA"
+                        }
+                    },
+                    {
+                        "nodeTo": "node2",
+                        "nodeFrom": "node0",
+                        "data": {
+                            "$color": "#557EAA"
+                        }
+                    }
+                ],
+                "data": {
+                    "$color": "#83548B",
+                    "$type": "star",
+                    "$dim": 40
+                },
+                "id": "node0",
+                "name": "ComputerScience"
+            },
+            {
+                "adjacencies": [
+                    {
+                        "nodeTo": "node0",
+                        "nodeFrom": "node1",
+                        "data": {
+                            "$color": "#557EAA"
+                        }
+                    }
+                ],
+                "data": {
+                    "$color": "#83548B",
+                    "$type": "square",
+                    "$dim": 10
+                },
+                "id": "node1",
+                "name": "Artificial Intelligence"
+            },
+            {
+                "adjacencies": [],
+                "data": {
+                    "$color": "#83548B",
+                    "$type": "circle",
+                    "$dim": 20
+                },
+                "id": "node2",
+                "name": "Computer Graphics"
+            },
+            {
+                "adjacencies": [
+                    {
+                        "nodeTo": "node1",
+                        "nodeFrom": "node3",
+                        "data": {
+                            "$color": "#557EAA"
+                        }
+                    }
+                ],
+                "data": {
+                    "$color": "#83548B",
+                    "$type": "circle",
+                    "$dim": 20
+                },
+                "id": "node3",
+                "name": "Deep Learning"
+            },
+            {
+                "adjacencies": [
+                    {
+                        "nodeTo": "node1",
+                        "nodeFrom": "node4",
+                        "data": {
+                            "$color": "#557EAA"
+                        }
+                    }
+                ],
+                "data": {
+                    "$color": "#83548B",
+                    "$type": "circle",
+                    "$dim": 20
+                },
+                "id": "node4",
+                "name": "Data Mining"
+            }
+        ];
+        // end
+        // init ForceDirected
+        var fd = new $jit.ForceDirected({
+            //id of the visualization container
+            injectInto: 'infovis',
+            //Enable zooming and panning
+            //by scrolling and DnD
+            Navigation: {
+                enable: true,
+                //Enable panning events only if we're dragging the empty
+                //canvas (and not a node).
+                panning: 'avoid nodes',
+                zooming: 10 //zoom speed. higher is more sensible
+            },
+            // Change node and edge styles such as
+            // color and width.
+            // These properties are also set per node
+            // with dollar prefixed data-properties in the
+            // JSON structure.
+            Node: {
+                overridable: true
+            },
+            Edge: {
+                overridable: true,
+                color: '#23A4FF',
+                lineWidth: 2.5
+            },
+            //Native canvas text styling
+            Label: {
+                type: labelType, //Native or HTML
+                size: 10,
+                style: 'bold'
+            },
+            //Add Tips
+            Tips: {
+                enable: true,
+                onShow: function(tip, node) {
+                    //count connections
+                    var count = 0;
+                    node.eachAdjacency(function() { count++; });
+                    //display node info in tooltip
+                    tip.innerHTML = "<div class=\"tip-title\">" + node.name + "</div>"
+                        + "<div class=\"tip-text\"><b>connections:</b> " + count + "</div>";
+                }
+            },
+            // Add node events
+            Events: {
+                enable: true,
+                type: 'Native',
+                //Change cursor style when hovering a node
+                onMouseEnter: function() {
+                    fd.canvas.getElement().style.cursor = 'move';
+                },
+                onMouseLeave: function() {
+                    fd.canvas.getElement().style.cursor = '';
+                },
+                //Update node positions when dragged
+                onDragMove: function(node, eventInfo, e) {
+                    var pos = eventInfo.getPos();
+                    node.pos.setc(pos.x, pos.y);
+                    fd.plot();
+                },
+                //Implement the same handler for touchscreens
+                onTouchMove: function(node, eventInfo, e) {
+                    $jit.util.event.stop(e); //stop default touchmove event
+                    this.onDragMove(node, eventInfo, e);
+                },
+                //Add also a click handler to nodes
+                onClick: function(node) {
+                    if(!node) return;
+                    // Build the right column relations list.
+                    // This is done by traversing the clicked node connections.
+                    var html = "<h4>" + node.name + "</h4><b> connections:</b><ul><li>",
+                        list = [];
+                    node.eachAdjacency(function(adj){
+                        list.push(adj.nodeTo.name);
+                    });
+                    //append connections information
+                    $jit.id('inner-details').innerHTML = html + list.join("</li><li>") + "</li></ul>";
+                }
+            },
+            //Number of iterations for the FD algorithm
+            iterations: 200,
+            //Edge length
+            levelDistance: 130,
+            // Add text to the labels. This method is only triggered
+            // on label creation and only for DOM labels (not native canvas ones).
+            onCreateLabel: function(domElement, node){
+                domElement.innerHTML = node.name;
+                var style = domElement.style;
+                style.fontSize = "0.8em";
+                style.color = "#ddd";
+            },
+            // Change node styles when DOM labels are placed
+            // or moved.
+            onPlaceLabel: function(domElement, node){
+                var style = domElement.style;
+                var left = parseInt(style.left);
+                var top = parseInt(style.top);
+                var w = domElement.offsetWidth;
+                style.left = (left - w / 2) + 'px';
+                style.top = (top + 10) + 'px';
+                style.display = '';
+            }
+        });
+        // load JSON data.
+        fd.loadJSON(json);
+        // compute positions incrementally and animate.
+        fd.computeIncremental({
+            iter: 40,
+            property: 'end',
+            onStep: function(perc){
+                Log.write(perc + '% loaded...');
+            },
+            onComplete: function(){
+                Log.write('done');
+                fd.animate({
+                    modes: ['linear'],
+                    transition: $jit.Trans.Elastic.easeOut,
+                    duration: 2500
+                });
+            }
+        });
+        // end
+    //}
+
 
 
 });
